1fb44f99d21f9d996e679f9a8840f5ce4cf1a210
Jason Hamm <jasonhamm.me@gmail.com>
2016-02-04 06:43:14 -0700

    Update app to use PROD config

--- server.js
+++ server.js
@@ -1,19 +1,39 @@
 var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+var path = require('path');
+var express = require('express');
+
 var PORT = process.env.PORT || 3000;
+var isDevelopment = (process.env.NODE_ENV !== 'production');
+var static_path = path.join(__dirname, 'public');
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true,
-  stats: {
-    colors: true
-  }
-}).listen(PORT, 'localhost', (err) => {
-  if (err) {
-    console.log(err);
-  }
+if(!isDevelopment) {
+  var WebpackDevServer = require('webpack-dev-server');
+  var config = require('./webpack.config');
 
-  console.log(`Listening at localhost:${PORT}`);
-});
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    stats: {
+      colors: true
+    }
+  }).listen(PORT, 'localhost', (err) => {
+    if (err) {
+      console.log(err);
+    }
+
+    console.log(`Listening at localhost:${PORT}`);
+  });  
+} else {
+  var app = express();
+
+  app.use(express.static(static_path))
+  .get('/', function (req, res) {
+    res.sendFile('index.html', {
+      root: static_path
+    });
+  }).listen(PORT, function (err) {
+    if (err) { console.log(err) };
+    console.log(`Listening at localhost:${PORT}`);
+  });
+}
---
+++ webpack-production.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      exclude: /node_modules/,
+      include: path.join(__dirname, 'src')
+    }, {
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, '..', '..', 'src')
+    }]
+  }
+};
