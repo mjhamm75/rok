b626ede673cacc8d9a21fa7d04ffd708057ce195
Jason Hamm <jasonhamm.me@gmail.com>
2016-02-03 21:35:56 -0700

    Remove unused reducers

--- src/reducers/dependent.info.js
+++
@@ -1,9 +0,0 @@
-export default function personalInfo(state = {}, action) {
-	switch(action.type) {
-		case "UPDATE_DEPENDENT_INFO":
-			state[action.key] = action.value;
-			return Object.assign({}, state);
-		default:
-			return state;
-	}
-}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,18 +1,8 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
-import personalInfo from './personal.info.js';
-import spouseInfo from './spouse.info.js';
-import dependentInfo from './dependent.info.js';
-import employmentInfo from './employment.info.js';
-import { routeReducer } from 'redux-simple-router'
 
 const rootReducer = combineReducers({
-  counter,
-  personalInfo,
-  spouseInfo,
-  dependentInfo,
-  employmentInfo,
-  routing: routeReducer
+  counter
 });
 
 export default rootReducer;
