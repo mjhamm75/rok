c95f5d299b1fb85bcd2b472ace9c2fd498644ef0
Jason Hamm <jasonhamm.me@gmail.com>
2016-02-04 07:38:34 -0700

    Reconfigure express server

--- server.js
+++ server.js
@@ -6,42 +6,36 @@ var PORT = process.env.PORT || 3000;
 var isDevelopment = (process.env.NODE_ENV !== 'production');
 var static_path = path.join(__dirname, 'public');
 
-// if(!isDevelopment) {
-//   var WebpackDevServer = require('webpack-dev-server');
-//   var config = require('./webpack.config');
+if(!isDevelopment) {
+  var WebpackDevServer = require('webpack-dev-server');
+  var config = require('./webpack.config');
 
-//   new WebpackDevServer(webpack(config), {
-//     publicPath: config.output.publicPath,
-//     hot: true,
-//     historyApiFallback: true,
-//     stats: {
-//       colors: true
-//     }
-//   }).listen(PORT, 'localhost', (err) => {
-//     if (err) {
-//       console.log(err);
-//     }
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    stats: {
+      colors: true
+    }
+  }).listen(PORT, 'localhost', (err) => {
+    if (err) {
+      console.log(err);
+    }
 
-//     console.log(`Listening at localhost:${PORT}`);
-//   });  
-// } else {
-//   var app = express();
+    console.log(`Listening at localhost:${PORT}`);
+  });  
+} else {
+  var app = express();
 
-//   app.use(express.static(static_path))
-//   .get('/', function (req, res) {
-//     res.sendFile('index.html', {
-//       root: static_path
-//     });
-//   }).listen(PORT, function (err) {
-//     if (err) { console.log(err) };
-//     console.log(`Listening at localhost:${PORT}`);
-//   });
-// }
+  app.use(express.static(__dirname + '/public'));
 
-var app = express();
+  app.get('*', function(req, res) {
+    res.sendFile('index.html', {
+      root: static_path
+    })  
+  })
 
-app.get('/', function(req, res) {
-  res.sendFile('index.html')
-}).listen(PORT, function(err) {
-  console.log(`Listening at port: ${PORT}`);
-});
+  app.listen(PORT, function(err) {
+    console.log(`Listening at port: ${PORT}`);
+  });
+}
