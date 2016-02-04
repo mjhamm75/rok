33fbf35a7796d6edf54516b63043422e5f97ce0a
Jason Hamm <jasonhamm.me@gmail.com>
2016-02-04 06:57:35 -0700

    Test express server

--- server.js
+++ server.js
@@ -6,34 +6,44 @@ var PORT = process.env.PORT || 3000;
 var isDevelopment = (process.env.NODE_ENV !== 'production');
 var static_path = path.join(__dirname, 'public');
 
-if(!isDevelopment) {
-  var WebpackDevServer = require('webpack-dev-server');
-  var config = require('./webpack.config');
+// if(!isDevelopment) {
+//   var WebpackDevServer = require('webpack-dev-server');
+//   var config = require('./webpack.config');
 
-  new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    hot: true,
-    historyApiFallback: true,
-    stats: {
-      colors: true
-    }
-  }).listen(PORT, 'localhost', (err) => {
-    if (err) {
-      console.log(err);
-    }
+//   new WebpackDevServer(webpack(config), {
+//     publicPath: config.output.publicPath,
+//     hot: true,
+//     historyApiFallback: true,
+//     stats: {
+//       colors: true
+//     }
+//   }).listen(PORT, 'localhost', (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
 
-    console.log(`Listening at localhost:${PORT}`);
-  });  
-} else {
-  var app = express();
+//     console.log(`Listening at localhost:${PORT}`);
+//   });  
+// } else {
+//   var app = express();
 
-  app.use(express.static(static_path))
-  .get('/', function (req, res) {
-    res.sendFile('index.html', {
-      root: static_path
-    });
-  }).listen(PORT, function (err) {
-    if (err) { console.log(err) };
-    console.log(`Listening at localhost:${PORT}`);
-  });
-}
+//   app.use(express.static(static_path))
+//   .get('/', function (req, res) {
+//     res.sendFile('index.html', {
+//       root: static_path
+//     });
+//   }).listen(PORT, function (err) {
+//     if (err) { console.log(err) };
+//     console.log(`Listening at localhost:${PORT}`);
+//   });
+// }
+
+var app = express();
+
+app.get('/', function(req, res) {
+  res.json({
+    jason: 'hamm'
+  })
+}).listen(PORT, function(err) {
+  console.log(`Listening at port: ${PORT}`);
+});
