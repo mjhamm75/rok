a9e660d23d483f2ae14cefb41c48751cbe3a60d4
Jason Hamm <jasonhamm.me@gmail.com>
2016-02-10 22:59:35 -0700

    Finish hero responsiveness

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Sticky from 'react-sticky';
 let hand = require('./../imgs/hand.png');
 let rokLogo = require('./../imgs/rok-logo.png');
 let rokTitle = require('./../imgs/rok-title.png');
@@ -7,23 +8,47 @@ require('!style!css!sass!./../sass/app.scss');
 class About extends Component {
 	render() {
 		return (
-			<div className="navimage">
-				<div className="navhero">
-					<div className="heroflex"> 
-						<div>
-							<img src={rokLogo} />
-						</div>
-						<div>
-							<img src={rokTitle} />
-						</div>
-						<div>
+			<div>
+				<div className="navimage">
+					<div className="navhero">
+						<div className="heroflex"> 
 							<div>
-								<div className="text">The Roots of Knowledge art glass window showcases humanitys pursuit of knowledge since the dawn of recorded time.  The documentary will transform the window into a live rendition of the journey of knowledge and will act as a megaphone to spread the message of the masterpiece to the world.</div>
-								<button>Sponser a piece of glass</button>
+								<img src={rokLogo} />
+							</div>
+							<div>
+								<img src={rokTitle} />
+							</div>
+							<div>
+								<div>
+									<div className="text">The Roots of Knowledge art glass window showcases humanitys pursuit of knowledge since the dawn of recorded time.  The documentary will transform the window into a live rendition of the journey of knowledge and will act as a megaphone to spread the message of the masterpiece to the world.</div>
+									<button>Sponser a piece of glass</button>
+								</div>
 							</div>
 						</div>
+			    	</div>
+				</div>
+				<Sticky>
+					<div className="navbar">
+						<div>
+							<a>Invisible</a>
+						</div>
+						<div>
+							<a href="/">The Roots of Knowledge Project</a>
+						</div>
+						<div>
+							<a href="/about">About the Project</a>
+						</div>
+						<div>
+							<a href="/donations">Donate</a>
+						</div>
+						<div>
+							<a href="/contact">Contact Us</a>
+						</div>
+						<div>
+							<a href="#">mailbox</a>
+						</div>
 					</div>
-		    	</div>
+				</Sticky>
 			</div>
 		)
 	}
