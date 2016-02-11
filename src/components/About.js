954f913f40c1a226f3063d382b9028e09d2ea276
Jason Hamm <jasonhamm.me@gmail.com>
2016-02-10 22:53:27 -0700

    Finish hero

--- src/components/About.js
+++ src/components/About.js
@@ -7,19 +7,20 @@ require('!style!css!sass!./../sass/app.scss');
 class About extends Component {
 	render() {
 		return (
-			<div className="image">
-				<img src={hand}/>
+			<div className="navimage">
 				<div className="navhero">
-					<div>
-						<img src={rokLogo} />
-					</div>
-					<div>
-						<img src={rokTitle} />
-					</div>
-					<div>
+					<div className="heroflex"> 
 						<div>
-							<div className="text">The Roots of Knowledge art glass window showcases humanitys pursuit of knowledge since the dawn of recorded time.  The documentary will transform the window into a live rendition of the journey of knowledge and will act as a megaphone to spread the message of the masterpiece to the world.</div>
-							<button>Sponser a piece of glass</button>
+							<img src={rokLogo} />
+						</div>
+						<div>
+							<img src={rokTitle} />
+						</div>
+						<div>
+							<div>
+								<div className="text">The Roots of Knowledge art glass window showcases humanitys pursuit of knowledge since the dawn of recorded time.  The documentary will transform the window into a live rendition of the journey of knowledge and will act as a megaphone to spread the message of the masterpiece to the world.</div>
+								<button>Sponser a piece of glass</button>
+							</div>
 						</div>
 					</div>
 		    	</div>
