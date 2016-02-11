befffde09896d29d032aad8b9b0732386eafefcc
Jason Hamm <jasonhamm.me@gmail.com>
2016-02-11 06:37:41 -0700

    Create link to donations page

--- src/components/About.js
+++ src/components/About.js
@@ -21,7 +21,7 @@ class About extends Component {
 							<div>
 								<div>
 									<div className="text">The Roots of Knowledge art glass window showcases humanitys pursuit of knowledge since the dawn of recorded time.  The documentary will transform the window into a live rendition of the journey of knowledge and will act as a megaphone to spread the message of the masterpiece to the world.</div>
-									<button>Sponser a piece of glass</button>
+									<a href="/donations">Sponser a piece of glass</a>
 								</div>
 							</div>
 						</div>
