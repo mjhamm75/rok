61d2ad3a50eeb763afb0a9b8cf704f90f9bb5f6f
Jason Hamm <jasonhamm.me@gmail.com>
2016-02-15 23:22:27 -0700

    update home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import Sticky from 'react-sticky';
 import Nav from './Nav';
+import Footer from './Footer';
 let hand = require('./../imgs/hand.png');
 let rokLogo = require('./../imgs/rok-logo.png');
 let rokTitle = require('./../imgs/rok-title.png');
 let glass = require('./../imgs/desktop.stainedglass.png');
-let twitter = require('./../imgs/twitter.png');
-let facebook = require('./../imgs/facebook.png');
-let pinterest = require('./../imgs/pinterest.png');
 
 let c1r1 = require('./../imgs/col1-1.png');
 let c1r2 = require('./../imgs/col1-2.png');
@@ -114,48 +112,7 @@ class Home extends Component {
 						<img src={c5r2} className="top5" />
 					</div>
 				</div>
-				<div className="footer">
-					<div>
-						<div>
-							Contact Us:
-							<br />
-							1-801-766-4111
-						</div>
-						<div>
-							3001 N Thanksgiving Way
-							<br />
-							Lehi, UT 84043
-						</div>
-					</div>
-					<div>
-						<div>
-							<Link to="about">About Us</Link>
-						</div>
-						<div>
-							<Link to="artists">Meet the Artists</Link>
-						</div>
-						<div>
-							<Link to="university">Utah Valley University</Link>
-						</div>
-						<div>
-							<Link to="donate">Donate</Link>
-						</div>
-					</div>
-					<div>
-						<div>
-							<img src={twitter}/>
-							<Link to="twitter">Twitter</Link>
-						</div>
-						<div>
-							<img src={facebook}/>
-							<Link to="facebook">Facebook</Link>
-						</div>
-						<div>
-							<img src={pinterest}/>
-							<Link to="pinterest">Pinterest</Link>
-						</div>
-					</div>
-				</div>
+				<Footer />
 				<div className="copywrite">
 					© 2016 Roots Media, LLC, & Holdman Studios, LLC, All Rights Reserved.
 				</div>
