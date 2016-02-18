5c04718cd77b6f6f56cc535365173ded11fedf5a
Jason Hamm <jasonhamm.me@gmail.com>
2016-02-17 18:21:12 -0700

    Questions to ask Emmy and Russ

--- questions.js
+++ questions.js
@@ -1,2 +1,24 @@
 Where to get the number of people donated?
 What is meteroirte?  Is it a mispelling?
+
+Where to host video from?
+	Youtube
+	Vimeo
+
+ross.wofley@gmail.com
+captcha
+	Check paypal, square guidelines for best practices
+
+spreadsheet with pagename and links for content
+
+Do the icons on the about page link out to anything?
+	Holdman Studios
+	UVU
+	Roots Media
+
+Where do we want the links in the footer to go to?
+
+When do we want the cart icon to show?  Always, or just when we have something in it.
+Where did you get the icon for the shopping cart?
+
+Hover state
