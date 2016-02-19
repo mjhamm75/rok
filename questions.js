d69ea9ea5cbe6fc7f4079280042440ee26d118ef
Jason Hamm <jasonhamm.me@gmail.com>
2016-02-18 22:11:34 -0700

    Update questions

--- questions.js
+++ questions.js
@@ -1,5 +1,4 @@
 Where to get the number of people donated?
-What is meteroirte?  Is it a mispelling?
 
 Where to host video from?
 	Youtube
@@ -11,25 +10,20 @@ captcha
 
 spreadsheet with pagename and links for content
 
-
-Do the icons on the about page link out to anything?
-	Holdman Studios
-	UVU
-	Roots Media
-
-Where do we want the links in the footer to go to?
-
-When do we want the cart icon to show?  Always, or just when we have something in it.
-Where did you get the icon for the shopping cart?
-
 Hover state
 
-Holdman Icon link out to Holdman Studios
-Meet the artist link to Holdman Studio Title
-Trade out Pinterest for Instagram
-
 Add piece - cut off sides until we hit breakpoint
-
-Change sponser a piece
-
 Send link to Heroku site
+
+
+F = Where did you get the icon for the shopping cart?
+F = What is meteroirte?  Is it a mispelling?
+F = Do the icons on the about page link out to anything?
+	F = Holdman Studios
+	F = UVU
+	F = Roots Media
+F = Where do we want the links in the footer to go to?
+F = Holdman Icon link out to Holdman Studios
+F = Meet the artist link to Holdman Studio Title
+F = Change sponser a piece
+F = Trade out Pinterest for Instagram
