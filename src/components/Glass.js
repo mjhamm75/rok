6b3827b23495fcf59e094e9752b861dd696405df
Jason Hamm <jasonhamm.me@gmail.com>
2016-02-24 20:17:39 -0700

    Add wall images

--- src/components/Glass.js
+++ src/components/Glass.js
@@ -1,8 +1,45 @@
 import React, { Component } from 'react';
 
+import b1 from './../imgs/b1.jpg'
+import b2 from './../imgs/b2.jpg'
+import b3 from './../imgs/b3.jpg'
+
+import c1 from './../imgs/c1.jpg'
+import c2 from './../imgs/c2.jpg'
+import c3 from './../imgs/c3.jpg'
+
+import d1 from './../imgs/d1.jpg'
+import d2 from './../imgs/d2.jpg'
+import d3 from './../imgs/d3.jpg'
+
+import e1 from './../imgs/e1.jpg'
+import e2 from './../imgs/e2.jpg'
+import e3 from './../imgs/e3.jpg'
+
 class Glass extends Component {
 	render() {
-		return <h3>Glass</h3>
+		return (
+			<div>
+				<div>
+					<img src={b1}/>
+					<img src={c1}/>
+					<img src={d1}/>
+					<img src={e1}/>
+				</div>
+				<div>
+					<img src={b2}/>
+					<img src={c2}/>
+					<img src={d2}/>
+					<img src={e2}/>
+				</div>
+				<div>
+					<img src={b3}/>
+					<img src={c3}/>
+					<img src={d3}/>
+					<img src={e3}/>
+				</div>
+			</div>
+		)
 	}
 }
 
