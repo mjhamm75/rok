ce0bdd9cd12ddba656032596124ce83f84a106fb
Jason Hamm <jasonhamm.me@gmail.com>
2016-02-24 20:47:16 -0700

    Style glass page

--- src/components/Glass.js
+++ src/components/Glass.js
@@ -16,17 +16,35 @@ import e1 from './../imgs/e1.jpg'
 import e2 from './../imgs/e2.jpg'
 import e3 from './../imgs/e3.jpg'
 
+import Nav from './Nav.js'
+
 require('!style!css!sass!./../sass/glass.scss');
 
 class Glass extends Component {
 	render() {
 		return (
 			<div className="glass">
-				<div>
-					<img src={b1}/>
-					<img src={c1}/>
-					<img src={d1}/>
-					<img src={e1}/>
+				<Nav fixed="true"/>
+				<div className="image">
+					<div>
+						<img src={b1}/>
+						<img src={c1}/>
+						<img src={d1}/>
+						<img src={e1}/>
+					</div>
+					<div>
+						<img src={b2}/>
+						<img src={c2}/>
+						<img src={d2}/>
+						<img src={e2}/>
+					</div>
+					<div>
+						<img src={b3}/>
+						<img src={c3}/>
+						<img src={d3}/>
+						<img src={e3}/>
+					</div>
+
 				</div>
 			</div>
 		)
