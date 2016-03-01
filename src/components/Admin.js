07caec67c8ed442070e7a184e277f418356cbd46
Jason Hamm <jasonhamm.me@gmail.com>
2016-02-29 21:14:17 -0700

    Add admin site

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+require('!style!css!sass!./../sass/admin.scss');
+
 export default class Admin extends Component {
 	render() {
 		return (
-			<h1>Admin</h1>
+			<div className="admin">
+				<h3>Email credentials</h3>
+
+			</div>
 		)
 	}
 }
