86980930a7038029f3a836993698ea53e7998b9c
Jason Hamm <jasonhamm.me@gmail.com>
2016-03-04 07:49:19 -0700

    Add react-skylight

--- src/components/Pick.js
+++ src/components/Pick.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import ImageMap from './ImageMap';
+import SkyLight from 'react-skylight';
 
 import coords from './coords.js';
 import b1 from './../imgs/b1.jpg'
 
+require('!style!css!sass!./../sass/pick.scss');
+
 class Pick extends Component {
+	addPiece() {
+		this.refs.simpleDialog.hide();
+	}
+
 	render() {
 		var glass = "glass";
 		return (
 			<div>
 				<h1>Pick</h1>
 				<ImageMap source={b1} mappingName={glass} coords={coords} selectArea={this.selectArea.bind(this)}/>
+				<button onClick={() => this.refs.simpleDialog.show()}>Open Modal</button>
+				<SkyLight 
+					ref="simpleDialog" 
+					title="Tree of Knowledge"
+					hideOnOverlayClicked>
+		          	<a className="sky-button" onClick={this.addPiece.bind(this)}>Add Piece</a>
+		        </SkyLight>
 			</div>
 		)
 	}
