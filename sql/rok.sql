d5a1276ff8fa27129995ffe19c8f5f3e6821ebdf
Jason Hamm <jasonhamm.me@gmail.com>
2016-03-03 21:21:45 -0700

    Keep sql commands to recreate

---
+++ sql/rok.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE ROK;
+CREATE USER rok WITH PASSWORD 'rok';
+CREATE TABLE EMAIL(id serial, username varchar(40), password varchar(40));
+GRANT ALL PRIVILEGES ON TABLE email TO rok;
+GRANT ALL PRIVILEGES ON SEQUENCE email_id_seq TO rok;
