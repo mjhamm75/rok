c181bd792982a3df8f6633020226a783cb866e4b
Jason Hamm <jasonhamm.me@gmail.com>
2016-03-03 20:54:06 -0700

    Connect to pg

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ var app = express();
 var compiler = webpack(config);
 var isDevelopment = (process.env.NODE_ENV !== 'production');
 
+var connectionString = process.env.PG_CONNECTION_STRING || 'postgres://rok:rok@localhost/rok';
+var knex = require('knex')({
+      client: 'pg',
+      connection: connectionString,
+      debug: false
+  });
+
 var emailServer  = email.server.connect({
    user:    "rootsofknowledgeproject", 
    password:"rootsofknowledge", 
