40ed2b4f0299974083e0dc7a086cb5296cd5216f
Jason Hamm <jasonhamm.me@gmail.com>
2016-03-05 23:04:39 -0700

    Call action to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { login } from './../actions/GlassActions';
 
 require('!style!css!sass!./../sass/login.scss');
 
-export default class Login extends Component {
+class Login extends Component {
 	render() {
 		return (
 			<div className="login">
-				<input placeholder=" Username"/>
-				<input placeholder=" Password"/>
-				<a>Login</a>
+				<input ref="username" placeholder=" Username"/>
+				<input ref="password" placeholder=" Password"/>
+				<a onClick={this.submitLogin.bind(this)}>Login</a>
 			</div>
 		)
 	}
+
+	submitLogin() {
+		var username = this.refs.username.value;
+		var password = this.refs.password.value;
+		this.props.dispatch(login(username, password));
+	}
 }
+
+function mapStateToProps(state) {
+	return {
+
+	}
+}
+
+export default connect(mapStateToProps)(Login);
