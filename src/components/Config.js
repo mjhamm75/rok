e9589760e20c55c074a1bae55ef0ac1131567563
Jason Hamm <jasonhamm.me@gmail.com>
2016-03-07 23:21:46 -0700

    Show errors if passwords are not the same

--- src/components/Config.js
+++ src/components/Config.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { updateEmailCreds, createNewUser } from './../actions/GlassActions';
+import className from 'classnames';
+require('!style!css!sass!./../sass/config.scss');
 
 class Config extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 	render() {
+		let emailError = className({
+			error: this.state.emailError
+		});
+		let showEmailError = this.state.emailError ? {
+			display: 'block'
+		} : {
+			display: 'none'
+		}
+		let userError = className({
+			error: this.state.userError
+		});
+		let showUserError = this.state.userError ? {
+			display: 'block'
+		} : {
+			display: 'none'
+		}
 		return (
 			<div>
 				<h2>Email Credentials</h2>
-				<div>
+				<div className={emailError}>
 					<div>
 						<div>Username</div>
 						<input ref="username"/>
@@ -20,22 +42,26 @@ class Config extends Component {
 						<div>Password</div>
 						<input ref="passwordValidation"/>
 					</div>
+					<div style={showEmailError}>Passwords are not the same</div>
 					<a onClick={this.updateEmailCreds.bind(this)}>Update Creds</a>
 				</div>
 				<h2>Add User</h2>
-					<div>
-						<div>Username</div>
-						<input ref="newuser"/>
+					<div className={userError}>
+						<div>
+							<div>Username</div>
+							<input ref="newuser"/>
+						</div>
+						<div>
+							<div>Password</div>
+							<input ref="newpassword"/>
+						</div>
+						<div>
+							<div>Password</div>
+							<input ref="newpasswordValidation"/>
+						</div>
+						<div style={showUserError}>Passwords are not the same</div>
+						<a onClick={this.createUser.bind(this)}>Create User</a>
 					</div>
-					<div>
-						<div>Password</div>
-						<input ref="newpassword"/>
-					</div>
-					<div>
-						<div>Password</div>
-						<input ref="newpasswordValidation"/>
-					</div>
-					<a onClick={this.createUser.bind(this)}>Create User</a>
 			</div>
 		)
 	}
@@ -44,21 +70,34 @@ class Config extends Component {
 		var username = this.refs.username.value;
 		var password = this.refs.password.value;
 		var passwordValidation = this.refs.passwordValidation.value;
-		if(password === passwordValidation) {
+		if(!password || password === passwordValidation) {
+			this.setState({
+				emailError: false
+			})
 			this.props.dispatch(updateEmailCreds(username, password));			
+			this.refs.username.value = '';
+			this.refs.password.value = '';
+			this.refs.passwordValidation.value = '';
+		} else {
+			this.setState({
+				emailError: true
+			})
 		}
-		this.refs.username.value = '';
-		this.refs.password.value = '';
-		this.refs.passwordValidation.value = '';
 	}
 
 	createUser() {
 		var username = this.refs.newuser.value;
 		var password = this.refs.newpassword.value;
 		var passwordValidation = this.refs.newpasswordValidation.value;
-		if(password === passwordValidation) {
+		if(!password ||password === passwordValidation) {
+			this.setState({
+				userError: false
+			})
 			this.props.dispatch(createNewUser(username, password));
 		}
+		this.setState({
+				userError: true
+		})
 		this.refs.newuser.value = '';	
 		this.refs.newpassword.value = '';
 		this.refs.newpasswordValidation.value = '';
