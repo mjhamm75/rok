255ed67de51c38f1d8d42722e1a75fb2428066b8
Jason Hamm <jasonhamm.me@gmail.com>
2016-03-11 00:14:53 -0700

    Update env variable name

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var connectionString = process.env.PG_CONNECTION_STRING || 'postgres://rok:rok@localhost/rok';
+var connectionString = process.env.DATABASE_URL || 'postgres://rok:rok@localhost/rok';
 var knex = require('knex')({
       client: 'pg',
       connection: connectionString,
