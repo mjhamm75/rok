e5ae307d5e8ba50db1664f312266ec0ffc842d5a
mjhamm75 <jason.hamm@ldschurch.org>
2017-01-14 11:45:00 -0700

    Finish converting login

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { login } from './../actions/GlassActions';
 
-require('!style!css!sass!./../sass/login.scss');
 import s from './Login.css';
 
 class Login extends Component {
