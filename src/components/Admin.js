5692000c747535794da6a80d04a47aebaedb6849
mjhamm75 <jason.hamm@ldschurch.org>
2017-01-14 11:53:25 -0700

    convert admin from sass to css modules

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 
-require('!style!css!sass!./../sass/admin.scss');
+import s from './Admin.css';
 
 export default class Admin extends Component {
 	render() {
 		return (
-			<div className="admin">
+			<div className={s.admin}>
 				<div>
-						<div className="flex">
-							<div>
-								<Link to="/admin/config">Config</Link>
-								<Link to="/admin/create-paths">Create Paths</Link>
-								<Link to="/admin/value-glass">Value Glass</Link>
+						<div className={s.content}>
+							<div className={s.buttons}>
+								<Link className={s.button} to="/admin/config">Config</Link>
+								<Link className={s.button} to="/admin/create-paths">Create Paths</Link>
+								<Link className={s.button} to="/admin/value-glass">Value Glass</Link>
 							</div>
 							{this.props.children}
 						</div>
