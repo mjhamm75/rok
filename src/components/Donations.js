46ec4be0ae2557ca6133422efde7d64b3ed574d9
mjhamm75 <jason.hamm@ldschurch.org>
2017-03-08 23:14:19 -0700

    Update text on donation page

--- src/components/Donations.js
+++ src/components/Donations.js
@@ -13,7 +13,7 @@ class Donations extends Component {
 				<div className={s.donations}>
 					<div className={s.hero}>
 						<div className={s.header}>Explore the wall and find a piece of glass to sponsor.</div>
-						<div className={s.blurb}>Enjoy the story along the way.</div>
+						<div className={s.blurb}>Each piece of glass can only be sponsored once.  Unless anonymity is desired, each donor will be recognized in the documentary credits</div>
 						<Link
 							className={s.button}
 							to="glass">Select a piece of glass</Link>
